8811ae8d41d8c6949ebbab3cfd6daa86715ee388
Artiom Choults <artiom.choults@pearlchain.net>
2019-04-08 22:01:09 +0200

    init project and gateway module

---
+++ gateway/src/main/java/k8s4spring/App.java
@@ -0,0 +1,13 @@
+package k8s4spring;
+
+/**
+ *
+ * Hello world!
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Gateway Developer!" );
+    }
+}
